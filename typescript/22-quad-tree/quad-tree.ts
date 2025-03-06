09390c3588c398d8113988eb44abe4539b46deea
Dreamless <selected.dreams@gmail.com>
2025-03-06 10:12:24 +0500

    TS-22 Construct Quad Tree Final

--- typescript/22-quad-tree/quad-tree.ts
+++ typescript/22-quad-tree/quad-tree.ts
@@ -1,20 +1,10 @@
-import { Node } from "./trees.js"
+import {Node} from "./trees.js"
 
 export function construct(grid: number[][]): Node {
   function buildTree(row: number, col: number, size: number): Node {
-    const val: number = grid[row][col];
-    let isSame = true;
-    for (let i = row; i < row + size; i++) {
-      for (let j = col; j < col + size; j++) {
-        if (grid[i][j] !== val) {
-          isSame = false;
-          break;
-        }
-      }
-    }
-
-    if (isSame) {
-      return new Node(Boolean(val), true);
+    const val = Boolean(grid[row][col]);
+    if (size === 1) {
+      return new Node(val, true)
     }
 
     const mid = size / 2;
@@ -23,6 +13,13 @@ export function construct(grid: number[][]): Node {
     const topRight: Node = buildTree(row, col + mid, mid);
     const bottomLeft: Node = buildTree(row + mid, col, mid);
     const bottomRight: Node = buildTree(row + mid, col + mid, mid);
+
+    const isAllLeaf: boolean = topLeft.isLeaf && topRight.isLeaf && bottomLeft.isLeaf && bottomRight.isLeaf;
+    const isAllSame: boolean = topLeft.val === topRight.val && topLeft.val === bottomLeft.val && topLeft.val === bottomRight.val;
+
+    if (isAllLeaf && isAllSame) {
+      return new Node(val, true);
+    }
 
     return new Node(true, false, topLeft, topRight, bottomLeft, bottomRight);
   }
