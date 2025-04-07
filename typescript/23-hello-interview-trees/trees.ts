c9763f718d03b63dd338db9f19a3091e5acae7b9
Dreamless <selected.dreams@gmail.com>
2025-04-07 10:10:22 +0500

    TS-23 Fixed TreeNode

--- typescript/23-hello-interview-trees/trees.ts
+++ typescript/23-hello-interview-trees/trees.ts
@@ -4,8 +4,8 @@ export class TreeNode {
   right: TreeNode | null
 
   constructor(val: number, left: TreeNode | null, right: TreeNode | null) {
-    this.val = val ?? 0;
-    this.left = left ?? null;
-    this.right = right ?? null;
+    this.val = val;
+    this.left = left;
+    this.right = right;
   }
 }
