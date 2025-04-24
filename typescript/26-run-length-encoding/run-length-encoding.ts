886edb770d36bf084916fa2c20542809674e1e26
Dreamless <selected.dreams@gmail.com>
2025-04-24 06:58:08 +0500

    TS-26 Run-Length Encoding

---
+++ typescript/26-run-length-encoding/run-length-encoding.ts
@@ -0,0 +1,41 @@
+export function encode(str: string): string {
+  if (str.length === 0) return "";
+
+  let newStr = "";
+  let count = 1;
+
+  for (let i = 0; i < str.length; i++) {
+    const currVal = str[i];
+    const nextVal = str[i + 1];
+
+    if (currVal === nextVal) {
+      count++;
+    } else {
+      newStr += (count > 1 ? count : "") + currVal;
+      count = 1;
+    }
+  }
+
+  return newStr;
+}
+
+export function decode(str: string): string {
+  let newStr = "";
+  const matches: RegExpMatchArray | null = str.match(/\d+.|./g);
+
+  if (matches === null) return ""
+
+  for (let i = 0; i < matches.length; i++) {
+    if (matches[i].length > 1) {
+      const currVal = matches[i].split('');
+      const letter: string = currVal.pop()!;
+      const num = Number(currVal.join(''));
+
+      newStr += letter.repeat(num);
+    } else {
+      newStr += matches[i];
+    }
+  }
+
+  return newStr;
+}
