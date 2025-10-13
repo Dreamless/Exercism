f3b77a73bc4ca963169bc106dd1bc768afea43ac
Dreamless <selected.dreams@gmail.com>
2025-10-13 17:06:44 +0500

    TS-44 Rewritten using regular expressions

--- typescript/44-pig-latin/pig-latin.ts
+++ typescript/44-pig-latin/pig-latin.ts
@@ -1,30 +1,14 @@
 export function translate(phrase: string): string {
-  const words: string[] = phrase.split(' ');
-
-  const translateWord = (word: string): string => {
-    const letters: string[] = ['a', 'e', 'i', 'o', 'u'];
-
-    if (letters.includes(word[0]) || word.startsWith('xr') || word.startsWith('yt')) {
-      return word + 'ay';
-    }
-
-    let firstSoundIndex = -1;
-    for (let i = 0; i < word.length; i++) {
-      const char = word[i];
-      if (letters.includes(char) || (char === 'y' && i > 0)) {
-        firstSoundIndex = i;
-        break;
+  const vowelRegex = /^(?:[aeiou]|xr|yt)/;
+  const replaceRegex = /^([^aeiou]*qu|[^aeiouy]+|y)(.*)/;
+  return phrase
+    .split(' ')
+    .map((word: string): string => {
+      if (vowelRegex.test(word)) {
+        return word + 'ay';
       }
-    }
 
-    if (firstSoundIndex > -1) {
-      const prefix = word.substring(0, firstSoundIndex);
-      const rest = word.substring(firstSoundIndex);
-      return `${rest}${prefix}ay`;
-    }
-
-    return word + 'ay';
-  };
-
-  return words.map(translateWord).join(' ');
+      return word.replace(replaceRegex, '$2$1ay');
+    })
+    .join(' ');
 }
