2f81b7fc3975e5b45449846af449e48030087d2b
Dreamless <selected.dreams@gmail.com>
2025-11-06 11:01:25 +0500

    TS-56 Handled BigInt cases

--- typescript/56-armstrong-numbers/armstrong-numbers.ts
+++ typescript/56-armstrong-numbers/armstrong-numbers.ts
@@ -1,9 +1,10 @@
 export function isArmstrongNumber(number: number | bigint): boolean {
   const numStr = String(number);
   const numDigits = numStr.length;
+  const exponent = BigInt(numDigits);
 
-  const sum = Array.from(numStr, Number)
-    .reduce((acc: number, digit: number): number => acc + (digit ** numDigits), 0);
+  const sum = Array.from(numStr, BigInt)
+    .reduce((acc, digit) => acc + (digit ** exponent), 0n);
 
   return sum === number;
 }
