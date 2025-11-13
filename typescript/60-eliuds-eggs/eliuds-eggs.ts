2d8926949bdcfa7d5dcf70b24eda02e67ad2e109
Dreamless <selected.dreams@gmail.com>
2025-11-13 18:25:29 +0500

    TS-60 Eliud's Eggs rewrote to bitwise right shift

--- typescript/60-eliuds-eggs/eliuds-eggs.ts
+++ typescript/60-eliuds-eggs/eliuds-eggs.ts
@@ -1,11 +1,15 @@
-export const eggCount = (displayValue: number): number => {
+export function eggCount(displayValue: number): number {
   let count = 0;
   let n = displayValue;
 
   while (n > 0) {
-    n = n & (n - 1);
-    count++;
+    if (n & 1) {
+      count += 1;
+    } else {
+      count += 0;
+    }
+    n >>>= 1;
   }
 
   return count;
-};
+}
